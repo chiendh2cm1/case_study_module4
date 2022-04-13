4cb1c9e778c54782daf8eead737112cbcdcbd592
chiendh2cm1 <chiendh2cm1@gmail.com>
2022-04-13 10:39:08 +0700

    add shops

---
+++ src/main/java/com/codegym/casestudymodule4/service/shop/IShopService.java
@@ -0,0 +1,8 @@
+package com.codegym.casestudymodule4.service.shop;
+
+import com.codegym.casestudymodule4.model.shop.Shop;
+import com.codegym.casestudymodule4.service.IGeneralService;
+
+public interface IShopService extends IGeneralService<Shop> {
+    Iterable<Shop> findShopByNameContaining(String name);
+}
